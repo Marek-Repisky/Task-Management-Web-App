8be855f4814330fe4566b3da1c67d0c8d4a4f71c
SomeUsername456 <marek.repisky55313@gmail.com>
2024-03-31 03:22:15 +0200

    Header and footer
    Added the first version of the footer and header, probably will change later

---
+++ partials/footer.php
@@ -0,0 +1,12 @@
+<footer>
+        <div class="footer-FRow">
+            <a href="mailto:menoRestauracie@gmail.com" target="_blank">Email</a>
+        </div>
+        <div>
+            <p>&copy; 2023 MenoRestauracie</p>
+            <p>Všetky práva vyhradené.</p>
+        </div>
+        <div class="footer-FRow">
+            <a href="tel:+421999888777" target="_blank">Telefón</a>
+        </div>
+    </footer>
---
+++ partials/header.php
@@ -0,0 +1,11 @@
+<ul class="topnav">
+        <li><a class="active" href="#">1. Podstranka</a></li>
+        <li><a href="#">2. Podstranka</a></li>
+        <li><a href="#">3. Podstranka</a></li>
+        <li class="right"><a href="#">4. Podstranka</a></li>
+      </ul>
+
+      <header>
+        <h1>To do List</h1>
+        <button class="button">Button</button>
+      </header>
