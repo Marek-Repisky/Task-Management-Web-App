4d3d4a6e36c2a6e2d167abc4bf308774a668c1a1
SomeUsername456 <marek.repisky55313@gmail.com>
2024-04-16 01:49:23 +0200

    List template
    Created a template for the list

--- partials/header.php
+++ partials/header.php
@@ -3,18 +3,13 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="style/style.com">
+    <link rel="stylesheet" href="css/style.css">
     <title>Nazov</title>
 </head>
 <body>
-<ul class="topnav">
-        <li><a class="active" href="#">1. Podstranka</a></li>
-        <li><a href="#">2. Podstranka</a></li>
-        <li><a href="#">3. Podstranka</a></li>
-        <li class="right"><a href="#">4. Podstranka</a></li>
-      </ul>
-
-      <header>
-        <h1>To do List</h1>
-        <button class="button">Button</button>
-      </header>
+  <ul class="topnav">
+    <li><a class="active" href="#">1. Podstranka</a></li>
+    <li><a href="#">2. Podstranka</a></li>
+    <li><a href="#">3. Podstranka</a></li>
+    <li class="right"><a href="#">4. Podstranka</a></li>
+  </ul>
---
+++ partials/pridat_prvok.php
@@ -0,0 +1,4 @@
+<section class="zoznam_riadok">
+    <div class="poradie">1.</div>
+    <textarea class="txtarea" name="ls" id="zoznam" cols="39" rows="1" placeholder="Prvok..."></textarea>
+</section>  
